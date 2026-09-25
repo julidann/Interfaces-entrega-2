e31a836bad27fcc01263ca669b6258893e199215
Julieta D'Annunzio <169107108+julidann@users.noreply.github.com>
2026-09-25 11:03:35 -0300

    Add profile menu behavior

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
 const menuButton = document.querySelector(".menu-button");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+const userButton = document.querySelector(".user-button");
+const profileMenu = document.querySelector("#profile-menu");
 
 menuButton.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("show");
+    profileMenu.classList.remove("show");
+});
+
+userButton.addEventListener("click", function () {
+    profileMenu.classList.toggle("show");
+    hamburgerMenu.classList.remove("show");
 });
